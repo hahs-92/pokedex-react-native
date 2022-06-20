364ef8dceaca46cf76beb3073e0c28b7c8b5083b
hahs <alexandre_2420@outlook.com>
2022-06-20 14:15:24 -0500

    [added]: getPokemonsDetail

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -2,10 +2,27 @@ const URl_BASE = 'https://pokeapi.co/api/v2/'
 
 export const getPokemons = async (limit=20, offset=0) => {
     try {
-        const url = `${URl_BASE}/pokemon?limit=${limit}&offset=${offset}`
+        const url = `${URl_BASE}/pokemon?limit=20&offset=0`
+        // const url = `${URl_BASE}/pokemon?limit=${limit}&offset=${offset}`
         const response = await fetch(url)
         const result = await response.json()
         return result
+    } catch (error) {
+        throw error
+    }
+}
+
+export const getPokemonDetail = async (url) => {
+    try {
+        const response = await fetch(url)
+        const result = await response.json()
+        return {
+            id:result.id,
+            name:result.name,
+            type:result.types[0].type.name,
+            order:result.order,
+            imagen:result.sprites.other['official-artwork'].front_default
+        }
     } catch (error) {
         throw error
     }
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,18 +1,41 @@
 import { View, Text, Button, StyleSheet } from 'react-native'
 import { useState, useEffect } from 'react';
 //api
-import { getPokemons } from '../../api/pokemon';
+import { getPokemons, getPokemonDetail } from '../../api/pokemon';
 //navigation nos lo provee stack-navigator
 export default function Home({navigation}) {
   const [pokemons, setPokemons] = useState([])
 
+  const getFetch = async() => {
+    try {
+      const response = await getPokemons()
+      const result = await response.results
+      const promises = await result.map(async(r) => getPokemonDetail(r.url))
+      const pokes = await Promise.all(promises)
+      setPokemons([...pokemons, ...pokes])
+
+      // getPokemons()
+      // .then(response => response.results)
+      // .then(pokemon => pokemon.map(poke => getPokemonDetail(poke.url)))
+      // .then(promises => Promise.all(promises))
+      // .then(data =>{
+      //   // setPokemons( [...data])
+      //   setPokemons([...pokemons, [...data]])
+      // })
+      // .catch(console.error)
+
+    } catch (error) {
+      console.log("error: ", error)
+    }
+  }
+
   useEffect(() => {
-    getPokemons()
-      .then(data => setPokemons(data))
-      .catch(console.error)
+    getFetch()
   },[])
 
+  console.log("----------------------------------------")
   console.log("data: ", pokemons)
+  console.log("length: ", pokemons.length)
 
   return (
     <View >
